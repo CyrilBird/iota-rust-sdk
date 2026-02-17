5d90fbae5da3c7f277b9d73de89638e358b393f6
CyrilBird <zhangjiong2004@gmail.com>
2026-02-17 16:33:48 +0800

    Add Upgradeable example

---
+++ bindings/go/examples/upgradeable_contracts/main.go
@@ -0,0 +1,3 @@
+package main
+import "fmt"
+func main() { fmt.Println("=== Upgradeable ===\n1.Proxy 2.Implementation 3.Storage") }
---
+++ bindings/python/examples/upgradeable_contracts.py
@@ -0,0 +1,6 @@
+from lib.iota_sdk import *
+import asyncio
+async def main():
+    print("=== Upgradeable ===\n1.Proxy 2.Implementation 3.Storage\n")
+if __name__ == "__main__":
+    asyncio.run(main())
